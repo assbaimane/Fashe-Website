a07df180f097863676983d7cc5ea0621cc26b618
Imane <assbaimane@hotmail.com>
2022-02-15 14:42:30 +0100

    Carousel finished but need good fonts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import './App.sass';
 import Header from './components/Header/Header'
-
+import Home from './components/pages/Home/Home'
 
 function App() {
   // --------------------------- DATA ------------------------
@@ -14,6 +14,7 @@ function App() {
   return (
     <div>
       <Header/>
+      <Home/>
     </div>
   );
 }
--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,16 +1,52 @@
-import './Cart.sass';
+import './Home.sass';
 import {useEffect} from 'react';
+import '../../../../node_modules/bootstrap/dist/js/bootstrap'
+import Slide1 from '../../../images/Home/Carousel/carousel-01.jpg.webp'
+import Slide2 from '../../../images/Home/Carousel/carousel-02.jpg.webp'
+import Slide3 from '../../../images/Home/Carousel/carousel-03.jpg.webp'
 
 
-function Home(props){
-    
+function Home(){
     // --------------------------- FUNCTIONS ------------------------
 
     //-------------------------- DISPLAY -------------------------
     return(
-        <div>
-
-        
+        //---------------------- CAROUSSEL -----------------------
+        <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src={Slide1} class="d-block w-100" alt="Jeans"></img>
+                    <div className="carousel-caption text-center text-light">
+                        <h1>Leather bags</h1>
+                        <p>New Collection</p>
+                        <button href="#" className='text-secondary bg-white'>SHOP NOW</button>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src={Slide2} class="d-block w-100" alt="Black sweat"></img>
+                    <div className="carousel-caption text-center text-light">
+                        <h1>Original sweats</h1>
+                        <p>New Collection</p>
+                        <button href="#" className='text-secondary bg-white'>SHOP NOW</button>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src={Slide3} class="d-block w-100" alt="Marine t-shirt"></img>
+                    <div className="carousel-caption text-center text-light">
+                        <h1>Lulu creation</h1>
+                        <p>New Collection</p>
+                        <button href="#" className='text-secondary bg-white'>SHOP NOW</button>
+                    </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
         </div>
     );
 }
