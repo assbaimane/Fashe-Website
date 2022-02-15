220593a4e365f3cd0e2159774c8c452a4261fbde
Imane <assbaimane@hotmail.com>
2022-02-15 13:55:11 +0100

    Navbar almost finished - Improvement on active element +href have to be done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,18 @@
 import './Header.sass';
+import Logo from '../../images/logo.png.webp'
 
-
-function Header(props){
-    //const
+function Header(){
 
     return(
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-expand-lg navbar-light bg-white">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#" onClick={()=> props.setCurrentPage("shop")}>flashe-website</a>
+                    <a className="navbar-brand" href="#"><img src={Logo} className="img-fluid"></img></a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
                         <li className="nav-item">
                         <a className="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
@@ -28,7 +27,7 @@ function Header(props){
                         </li>
                     </ul>
                     </div>
-                    <button>Fav</button>
+                    <button><i class="fa-solid fa-heart"></i></button>
                     <button><i className="fa-solid fa-bag-shopping"></i></button>
                 </div>
             </nav>
