6eab5da75b3ef26d1bdacfcd87d07c321ae929c2
Imane <assbaimane@hotmail.com>
2022-02-18 00:39:14 +0100

    Feat : Cart Page : add items in cart (onclick in products) - 2 level props -IA

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,13 +1,23 @@
 import './Card.sass';
+import {useEffect, useState} from 'react';
+import Products from '../pages/Products/Products';
 
 function Card(props){
-    return(
+    // --------------------------- VARIABLES ------------------------
+    // const [shopBag, setShopBag] = useState([])  
 
+    // --------------------------- FUNCTIONS ------------------------
+    // const addToBag = (element) =>{
+    //     console.log("Heyyy, tu as ajouté ça à ton panier " + element)
+    // }
+
+    // ----------------------------- VIEW ---------------------------
+    return(
         <div className="mx-auto my-auto" className={props.display}>
             <div className="product-card">
                 <img className="img-fluid" src={props.img} alt={props.title} />
                 <div id="card-hover">
-                    <button id='add-to-cart'>Add to Cart</button>
+                    <button id='add-to-cart' onClick={(param) => props.addToBag(props.item)}>Add to Cart</button>
                     <button id='heart'><i className="fa-solid fa-heart"></i></button>
                 </div>
             </div>
