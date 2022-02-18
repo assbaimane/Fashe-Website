0822e937281b15bea55a24f2d1d9300a999fb790
Amanda <amandaksailer@gmail.com>
2022-02-18 15:04:15 +0100

    cards beautification, responsivity and corner span -aks

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,27 +6,26 @@ import Products from '../pages/Products/Products';
 function Card(props){
     // --------------------------- VARIABLES ------------------------
     // const [shopBag, setShopBag] = useState([])  
-    // const [productStatus, setSpan] = useState("");
 
     // --------------------------- FUNCTIONS ------------------------
     // const addToBag = (element) =>{
     //     console.log("Heyyy, tu as ajouté ça à ton panier " + element)
     // }
-    // const productState = (productStatus) => {
-    //     setSpan(productStatus)
-    // };
 
     // ----------------------------- VIEW ---------------------------
     return(
         <div className="products mx-auto my-auto" className={props.display}>
             <div className="product-card">
                 {/* product status button */}
-                {/* <span>{props.stat}=productState</span> */}
+                {props.item['state'] === "New" || props.item['state'] === "Old" ? <span>{props.item['state']}</span> : null }
+
                 {/* product photo */}
                 <img className="img-fluid" src={props.img} alt={props.title} />
                 <div id="card-hover">
                     {/* add to cart button */}
-                    <button id='add-to-cart' onClick={(param) => props.addToBag(props.item)}>Add to Cart</button>
+                    <div className="btnContainer">
+                        <button id='addInCart' onClick={(param) => props.addToBag(props.item)}>Add to Cart</button>
+                    </div>
 
                     {/* like button */}
                     <button id='heart'><i className="fa-solid fa-heart"></i></button>
