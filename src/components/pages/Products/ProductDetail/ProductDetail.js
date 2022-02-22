5b479719088b5e73c5e30dbdfc76dc807367c5bd
Amanda <amandaksailer@gmail.com>
2022-02-22 09:06:16 +0100

    yesterday's progress on product detail -aks

--- src/components/pages/Products/ProductDetail/ProductDetail.js
+++ src/components/pages/Products/ProductDetail/ProductDetail.js
@@ -1,8 +1,8 @@
 import './ProductDetail.sass';
-import { useParams } from 'react-router-dom';
+// import { useParams } from 'react-router-dom';
 import { useState } from 'react';
 
-function ProductDetail(src, name, price, description) {
+function ProductDetail(props) {
     //-------------------------- VARIABLES -------------------------
 
     //-------------------------- FUNCTIONS -------------------------
@@ -23,47 +23,53 @@ function ProductDetail(src, name, price, description) {
 
     return (
         <>
-            <div className="row">
-                <div className="product-details-image col-12 col-md-4">
-                    {/* <img src="{elements.Product.src}" alt="" /> */}
-                    <img src="{Products.src}" alt="" />
+            <div id='product-detail-page' className="row">
+                <div className="product-details-image  col-12 col-md-4">
+                    <img className='img-fluid' src={`../${props.selectedProduct.src}`} alt="error" />
                 </div>
                 <div className="product-details-info col-12 col-md-4">
                     {/* // props.selectedProduct is the product you clicked on, props.selectedProduct.key permite you to read the value */}
                     
-                    <h1 className='product-details-name'>{Products.name}</h1>
-                    <h2 className='product-details-price'>{Products.price}</h2>
-                    <p className='product-details-description'>{Products.description}</p>
+                    <h1 className='product-details-name'>{props.selectedProduct.name}</h1>
+                    <p className='product-details-description'>
+                        {props.selectedProduct.description}</p>
                     <div className="product-details-color dropdown">
-                        <button class="btn btn-* dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                        <label className='color-container'>
+
+                        </label>
+                        <button className="btn btn-* dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
                             Color
                         </button>
-                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                            <li><a class="dropdown-item" href="#">White</a></li>
-                            <li><a class="dropdown-item" href="#">Black</a></li>
-                            <li><a class="dropdown-item" href="#">Blue</a></li>
+                        <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                            <li><a className="dropdown-item" href="#">White</a></li>
+                            <li><a className="dropdown-item" href="#">Black</a></li>
+                            <li><a className="dropdown-item" href="#">Blue</a></li>
                         </ul>
                     </div>
                     <div className="product-details-size">
-                        <button class="btn btn-* dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                        <button className="btn btn-* dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
                             Size
                         </button>
-                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                            <li><a class="dropdown-item" href="#">XS</a></li>
-                            <li><a class="dropdown-item" href="#">S</a></li>
-                            <li><a class="dropdown-item" href="#">M</a></li>
-                            <li><a class="dropdown-item" href="#">L</a></li>
-                            <li><a class="dropdown-item" href="#">XL</a></li>
+                        <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                            <li><a className="dropdown-item" href="#">XS</a></li>
+                            <li><a className="dropdown-item" href="#">S</a></li>
+                            <li><a className="dropdown-item" href="#">M</a></li>
+                            <li><a className="dropdown-item" href="#">L</a></li>
+                            <li><a className="dropdown-item" href="#">XL</a></li>
                         </ul>
 
                     </div>
-                    <div className="product-details-quantity">
-                        <button className='quantity-button' onClick={decreaseQuantity}>-</button>
-                        <span>{quantity}</span>
-                        <button className='quantity-button' onClick={increaseQuantity}>+</button>
-                    </div>
-                    <div className="product-detail-add">
-                        <button>Add to Cart</button>
+                    <div className="finalize-product col-12">
+                        <div className="row">
+                            <div className="product-details-quantity col-md-6 col-12">
+                                <button className='quantity-button' onClick={decreaseQuantity}>-</button>
+                                <span>{quantity}</span>
+                                <button className='quantity-button' onClick={increaseQuantity}>+</button>
+                            </div>
+                            <div className='col-md-6 col-12'>
+                                <button id="product-detail-add" type="submit">Add to Cart</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
